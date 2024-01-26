c5c5a0977713e9ae71c22803d5053c7683d4b570
Nedzib Sastoque <bsastoque@buk.co>
2024-01-26 10:39:51 -0500

    first test

---
+++ .github/scripts/integration-script.js
@@ -0,0 +1,66 @@
+const { Client } = require('@notionhq/client');
+
+console.log("Notion Token: ", process.env.TU_TOKEN_DE_NOTION)
+console.log("Notion Database ID: ", process.env.ID_DE_TU_BASE_DE_DATOS_EN_NOTION)
+
+// Configura el cliente de Notion con tu token de integración
+const notion = new Client({ auth: process.env.TU_TOKEN_DE_NOTION });
+
+// Función para agregar un nuevo caso borde a la base de datos de Notion
+async function agregarCasoBorde(titulo, url, cuerpo) {
+    try {
+        const response = await notion.pages.create({
+            parent: {
+                database_id: process.env.ID_DE_TU_BASE_DE_DATOS_EN_NOTION,
+            },
+            properties: {
+                Titulo: {
+                    title: [
+                        {
+                            text: {
+                                content: titulo,
+                            },
+                        },
+                    ],
+                },
+                URL: {
+                    url: url,
+                },
+            },
+            children: [
+                {
+                    object: 'block',
+                    type: 'paragraph',
+                    paragraph: {
+                        text: [
+                            {
+                                type: 'text',
+                                text: {
+                                    content: cuerpo,
+                                },
+                            },
+                        ],
+                    },
+                },
+            ],
+        });
+
+        console.log('Caso borde agregado a Notion:', response.id);
+    } catch (error) {
+        console.error('Error al agregar caso borde a Notion:', error.body);
+    }
+}
+
+
+// Obtén datos del evento de GitHub
+const eventoGitHub = process.argv[2];
+console.log('Evento de GitHub:', eventoGitHub);
+const tituloPR = process.argv[3];
+console.log('Título del PR:', tituloPR);
+const urlPR = process.argv[4];
+console.log('URL del PR:', urlPR);
+const cuerpoPR = process.argv[5];
+console.log('Cuerpo del PR:', cuerpoPR);
+const cuerpoNotion = cuerpoPR.split(/\r?\n/).map(line => `- ${line}`).join('\n');
+
+agregarCasoBorde(tituloPR, urlPR, cuerpoNotion);
